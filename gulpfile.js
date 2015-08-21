a4a64fb99ad6ad2b0999b308b757b05217c7c030
Jay Perry <j@jayperryworks.com>
2015-08-20 21:50:38 -0400

    gulp svg sprite workflow setup

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var gulp = require("gulp");
+
+// useful file paths
+var icons = {
+  src  : "source/assets/images/icons",
+  dest : "source/assets/images"
+};
+
+var fileName = "spritemap.svg";
+
+// tell me what the error is!
+// -> prevent .pipe from dying on error w/ gulp-plumber
+// -> and give more useful error messages
+var showError = function(err) {
+  $.util.beep();
+  console.log(err);
+};
+
+// load plugins
+var $ = require("gulp-load-plugins")();
+
+gulp.task("default", ["icons"]);
+
+gulp.task("icons", function() {
+  return gulp.src(icons.src + "/*.svg")
+             .pipe($.plumber({
+                errorHandler: showError
+             }))
+             .pipe($.svgmin())
+             .pipe($.svgstore({
+                fileName: fileName,
+                prefix: ""
+             }))
+             .pipe($.cheerio(function($) {
+                    // add "display: none" to root svg tag
+                    $('svg').attr('style', 'display:none');
+                    // remove 'fill' & 'stroke' attributes from everything
+                    // -> replace in CSS
+                    $('svg').find('[fill]').removeAttr('fill');
+                    $('svg').find('[stroke]').removeAttr('stroke');
+                    $('svg').find('desc').remove();
+             }))
+             .pipe(gulp.dest(icons.dest + "/"));
+});
