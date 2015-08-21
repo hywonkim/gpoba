e22356aed3b7c154b32f6a8cd4f791417e42f610
Jay Perry <j@jayperryworks.com>
2015-08-20 21:44:12 -0400

    wiring up bower modules, JS libs, scss libs

--- config.rb
+++ config.rb
@@ -3,9 +3,10 @@
 ###
 
 # Change Compass configuration
-# compass_config do |config|
+compass_config do |config|
+  require "compass/import-once/activate"
 #   config.output_style = :compact
-# end
+end
 
 ###
 # Page options, layouts, aliases and proxies
@@ -28,6 +29,12 @@
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+# Pretty URLs (ie about.html.erb => about/index.html)
+activate :directory_indexes
+page "README.md", :directory_index => false
+page "LICENSE", :directory_index => false
+page "404.html", :directory_index => false
+
 ###
 # Helpers
 ###
@@ -36,9 +43,9 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -47,25 +54,40 @@
 #   end
 # end
 
-set :css_dir, 'css'
+set :site_title, 'GPOBA Annual Report 2015'
 
-set :js_dir, 'js'
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+set :fonts_dir, 'assets/fonts'
 
-set :images_dir, 'img'
+# Add bower's directory to sprockets asset path
+# -> use bundler for back-end dependencies, bower for front-end dependencies
+# -> http://dejimata.com/2013/11/4/bundler-meet-bower
+after_configuration do
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  sprockets.append_path File.join root, @bower_config["directory"]
+
+  sprockets.import_asset 'jquery'
+  sprockets.import_asset 'modernizr/modernizr.js'
+end
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  # Minify HTML on build
+  activate :minify_html
 
   # Enable cache buster
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
