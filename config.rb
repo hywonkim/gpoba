1955e0d2939498095a08a4d427056a2cd75c1ce5
Jay Perry <j@jayperryworks.com>
2015-09-04 18:20:06 -0400

    configured blog extension

--- config.rb
+++ config.rb
@@ -22,12 +22,18 @@ end
 #
 # A path which all have the same layout
 with_layout :chapter do
-  page "/sections/*"
+  page "/content/*"
 end
 
 # Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
+
+# set options on blog
+activate :blog do |blog|
+    blog.prefix = "content"
+    blog.permalink = "{chapter}.html"
+end
 
 # Pretty URLs (ie about.html.erb => about/index.html)
 activate :directory_indexes
