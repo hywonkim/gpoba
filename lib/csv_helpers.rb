77adb356028be10baa8ebbbc8af2bcf95f2bd7b0
Jay Perry <j@jayperryworks.com>
2015-10-28 11:48:44 -0400

    table sector icons

--- lib/csv_helpers.rb
+++ lib/csv_helpers.rb
@@ -12,43 +12,43 @@ class CSV_Helpers < Middleman::Extension
     end
 
     def table(file, html_class = '', highlight_rows= nil, table_title = nil, column_label = nil, column_icon = nil)
-      
+
       # Create the Table object from CSV File
         csv_data = File.read(File.join(data_dir, '/tables/'+ file)) # TODO potentially make it dynamically find the location. For now all tables must be added to the data/tables folder.
         csv = CSV.new(csv_data, :headers => true)
         tbl_obj = csv.read
 
-      # Build HTML Table              
+      # Build HTML Table
         table_title_class = 'titled_table' if table_title
         html_table = "<table class=\"tablesaw #{html_class} #{table_title_class}\" data-tablesaw-mode=\"swipe\" >"
 
-        # Adds table title 
+        # Adds table title
         html_table.prepend("<h5 class=\"table_title\">#{table_title}</h5>") if table_title
 
         # Build Headers
         html_table += "<thead><tr>"
 
-        indexed_headers = Array.new 
+        indexed_headers = Array.new
         tbl_obj.headers.each_with_index do |header, index|
           header_string = header
           indexed_headers[index] = header_string
           persist = 'data-tablesaw-priority="persist"' if index == 0
           html_table += "<th #{persist}> #{ header_string } </th>"
         end
-                
+
         html_table += "</tr></thead>"
 
         # Build Rows
-        tbl_obj.each_with_index do |row, index|                  
+        tbl_obj.each_with_index do |row, index|
           if highlight_rows
             html_table += "<tr class=\"row-#{index}\">"
-          else 
+          else
             html_table += "<tr>"
           end
-          
-          row.to_hash.values.each_with_index do |value, index|             
+
+          row.to_hash.values.each_with_index do |value, index|
             header_label = indexed_headers[index]
-            
+
             # Adds a class for all row elements in a column
             if column_label && index == (column_label - 1)
               column_class = 'column_label'
