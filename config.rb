bca03a660d78f2bd35e0f558300e8aae9c69dc75
Scott Schaffter <scott@bivee.co>
2015-10-30 19:28:52 -0400

    update for heroku

--- config.rb
+++ config.rb
@@ -162,7 +162,7 @@ after_configuration do
 end
 
 # For heroku use https else default to http
-ENV['APP_DOMAIN'] == 'production' ? http = 'https' : http = 'http'
+ENV['RACK_ENV'] == 'production' ? http = 'https' : http = 'http'
 
 # For Heroku Builds
 set :url_root, "#{http}://#{ENV['APP_DOMAIN'] ? ENV['APP_DOMAIN'] : 'localhost:4567'}"
